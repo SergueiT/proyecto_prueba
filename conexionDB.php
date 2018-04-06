173dc36a536f1cae51c7342fad0974157b7bbfe8
SergueiT <38143898+SergueiT@users.noreply.github.com>
2018-04-06 15:24:17 -0600

    modifique base
    base de datos modificada

--- conexionDB.php
+++ conexionDB.php
@@ -1,7 +1,7 @@
 
 <?php
 //conexion nueva de base de datos
-$mysql = new mysqli("localhost", "root", "", "mydb");
+$mysql = new mysqli("localhost", "root", "", "Pruebas_de_todo");
 if ($mysql->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysql->connect_errno . ") " . $mysql->connect_error;
 }
